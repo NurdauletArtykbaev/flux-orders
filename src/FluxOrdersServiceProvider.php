20564dadc96d2089989c41c1dc9bbeee4923429d
Nurdaulet <artykbaev.nurdaulet@mail.ru>
2023-11-01 11:31:17 +0600

    fix migrations

--- src/FluxOrdersServiceProvider.php
+++ src/FluxOrdersServiceProvider.php
@@ -48,21 +48,20 @@ class FluxOrdersServiceProvider extends ServiceProvider
 
     protected function publishMigrations()
     {
-
         $this->publishes([
-            __DIR__ . '/../database/migrations/check_cancel_reasons_table.php.stub' => $this->getMigrationFileName('check_cancel_reasons_table.php'),
-            __DIR__ . '/../database/migrations/check_payment_methods_table.php.stub' => $this->getMigrationFileName('check_payment_methods_table.php'),
-            __DIR__ . '/../database/migrations/check_orders_table.php.stub' => $this->getMigrationFileName('check_orders_table.php'),
-            __DIR__ . '/../database/migrations/check_canceled_orders_table.php.stub' => $this->getMigrationFileName('check_canceled_orders_table.php'),
-            __DIR__ . '/../database/migrations/check_verify_issued_order.php.stub' => $this->getMigrationFileName('check_verify_issued_order.php'),
-            __DIR__ . '/../database/migrations/check_verify_return_order.php.stub' => $this->getMigrationFileName('check_verify_return_order.php'),
+            __DIR__ . '/../database/migrations/check_cancel_reasons_table.php.stub' => $this->getMigrationFileName(0,'check_cancel_reasons_table.php'),
+            __DIR__ . '/../database/migrations/check_payment_methods_table.php.stub' => $this->getMigrationFileName(1,'check_payment_methods_table.php'),
+            __DIR__ . '/../database/migrations/check_orders_table.php.stub' => $this->getMigrationFileName(2,'check_orders_table.php'),
+            __DIR__ . '/../database/migrations/check_canceled_orders_table.php.stub' => $this->getMigrationFileName(3,'check_canceled_orders_table.php'),
+            __DIR__ . '/../database/migrations/check_verify_issued_order.php.stub' => $this->getMigrationFileName(4,'check_verify_issued_order.php'),
+            __DIR__ . '/../database/migrations/check_verify_return_order.php.stub' => $this->getMigrationFileName(5,'check_verify_return_order.php'),
         ], 'flux-base-migrations');
     }
 
     /**
      * Returns existing migration file if found, else uses the current timestamp.
      */
-    protected function getMigrationFileName(string $migrationFileName): string
+    protected function getMigrationFileName($index,string $migrationFileName): string
     {
         $timestamp = date('Y_m_d_His');
 
@@ -70,7 +69,7 @@ class FluxOrdersServiceProvider extends ServiceProvider
 
         return Collection::make([$this->app->databasePath() . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR])
             ->flatMap(fn($path) => $filesystem->glob($path . '*_' . $migrationFileName))
-            ->push($this->app->databasePath() . "/migrations/{$timestamp}_{$migrationFileName}")
+            ->push($this->app->databasePath() . "/migrations/{$timestamp}{$index}_{$migrationFileName}")
             ->first();
     }
 }
